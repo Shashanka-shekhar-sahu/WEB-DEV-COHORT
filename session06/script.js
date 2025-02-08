68b98f3da2b165ea403befe155c75083f88e4a50
shashank <situlsahu007@gmail.com>
2025-02-08 12:29:31 +0530

    added new code

---
+++ session06/script.js
@@ -0,0 +1,26 @@
+console.log("hello world!!!")
+
+
+function print(){
+    console.log("hello!!!")
+}
+function print01(){
+    console.log("hii!!!")
+}
+
+print01(); //first print this 
+print();   // then print this
+
+
+
+
+function bringTea(numberOfTea){
+    console.log(`bring ${numberOfTea} tea`)
+}
+bringTea(2);
+
+function addTwo(num1,num2){
+    return (num1+num2)
+}
+
+console.log(addTwo(2,2));
