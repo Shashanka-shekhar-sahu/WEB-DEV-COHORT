4cf6dadf343831a8fd1464acefed81672c7e4906
shashank <situlsahu007@gmail.com>
2025-02-17 23:28:18 +0530

    question & answer

--- session08/script04.js
+++ session08/script04.js
@@ -37,6 +37,7 @@ s2.lName = 'Sahu'
 console.log(s1) // { fName: 'situl', lName: 'sahu' }
 console.log(s2) // { fName: 'Shashank', lName: 'Sahu' }
 
+// serilization and deserilization
 // spread operator
 //a lots of key here and we will copy all the key in a different object that time we will use spread operator.
 
@@ -46,7 +47,43 @@ let x1 = {
 }
 
 let x2 = {
-   ...x1 // // spread operator
+   ...x1 // // spread operator(shallow copy, pass by referance)
 }
 console.log(x1) // { fName: 'Situl', lName: 'Sahu' }
 console.log(x2) // { fName: 'Situl', lName: 'Sahu' }
+
+// deep copy(pass by value)
+// first option
+let z1 = {lName : 'abc',
+           address :{
+            h: 1,
+            s:2
+           }
+}
+
+let z2 = {
+    ...z1,
+    address :{
+        ...z1
+    }
+}
+console.log(z1) //{ lName: 'abc', address: { h: 1, s: 2 } }
+console.log(z2) //{ lName: 'abc', address: { lName: 'abc', address: { h: 1, s: 2 } } }
+
+
+
+// second option
+let y1 = {lName : 'abc',
+    address :{
+     h: 1,
+     s:2
+    }
+}
+const y1ConvertToString = JSON.stringify(y1) // convert objects key to string 
+console.log(y1ConvertToString) //{"lName":"abc","address":{"h":1,"s":2}}
+let y2 = JSON.parse(y1ConvertToString)
+y2.fName = 'jyoti'
+p2.address = 'odisha'
+console.log(y1) //{ lName: 'abc', address: { h: 1, s: 2 } }
+console.log(y2) //{ lName: 'abc', address: { h: 1, s: 2 }, fName: 'jyoti' }
+
--- session09/script01.js
+++ session09/script01.js
@@ -1,12 +1,68 @@
-Function.prototype.describe = function(){
-    console.log('Function name is ${this.name}')
+// Function.prototype.describe = function(){
+//     console.log('Function name is ${this.name}')
+// }
+
+// function greet(name){
+//     return 'Hello ${name}'
+// }
+
+// greet.describe()
+
+
+
+
+//Question & Answer
+// 01. Create an object representing a type of tea with properties for name, type and caffeine content.
+const teas = {
+    name : "lemon tea",
+    type : "green",
+    caffine : "low"
+}
+console.log(teas)
+
+// 02. Access and print the name and type properties of the tea object.
+console.log(teas.name)
+console.log(teas["type"])
+
+//03. Add a new property otigin to the tea object.
+teas.origin = "assam tea"
+
+
+//04. Change the cafeine level of the tea object to "Medium".
+teas.caffine = "medium"
+
+//05. Remove the type property from the tea object.
+delete teas.type
+
+//06. Check if the tea object has a property origin.
+console.log("origin" in teas)
+
+//07. Use a for...in loop to print all properties of the tea object.
+for (let key in teas) {
+    console.log(key + ":" + teas[key])
 }
 
-function greet(name){
-    return 'Hello ${name}'
+//08. Create a nested object representing different types of teas and their properties.
+const myTeas = {
+    greentea: {
+        name : "green tea",
+        color : "green",
+        cups : {
+            one: "1",
+            two: "2"
+        }
+    },
+    blacktea: {
+         name : "black tea",
+         color : "black"
+    }
 }
 
-greet.describe()
+//09. Create a copy of the tea object.
+const teaCopy = {...teas} // shallow copy
 
+const anotherCopy = teas // (reference pass, not a copy)
+
+ 
 
 
